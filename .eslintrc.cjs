0fd4246dd22f60400cadc5d45d3bb0393b18f4fb
arief2020 <msyaifullahalarief@gmail.com>
2024-04-02 22:58:24 +0700

    setup react husky

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb', 'prettier'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+  },
+  plugins: ['react'],
+  settings: {
+    react:{
+      version: 'detect'
+    }
+  }
+};
